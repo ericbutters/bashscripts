b3c99d93863f9659962a3c34f909720af5fe0c5f
dkuhne <daniel.kuhne@s1nn.de>
2014-01-17 14:48:27 +0100

    logmem: show active values

--- logMem.sh
+++ logMem.sh
@@ -1,26 +1,46 @@
 #!/bin/sh
 
-STATUS=$1
+PARAMETER=$1
 PAGE=4
 DIV=1024
 #PIDLIST=$(cat /tmp/pid.list)
 PIDLIST=$(ls /proc | grep -v grep | grep '^[0-9]\{1,5\}$')
-LASTMEM=$(cat /proc/meminfo | grep -w MemFree | awk {'print $2'})
-LASTMEM=$(($LASTMEM / $DIV))
 
-LASTDMESGLN=$(dmesg | tail -n 1)
+case $PARAMETER in
+"buffers")
+	TRIGGER="Buffers"
+	;;
+*)
+	TRIGGER="MemFree"
+	;;
+esac
+
+LAST=$(cat /proc/meminfo | grep -w $TRIGGER: | awk {'print $2'})
+if [ "$TRIGGER" != "Buffers" ]; then
+	LAST=$(($LAST / $DIV))
+fi
 
 while [ 1 ]; do
 #	PIDLIST=$(ls /proc | grep -v grep | grep '^[0-9]\{1,4\}$')
-	CACHE=$(cat /proc/meminfo | grep -w Cached | awk {'print $2'})
-	MEMFREE=$(cat /proc/meminfo | grep -w MemFree | awk {'print $2'})
-	MEMFREE=$(($MEMFREE / $DIV))
-	BUFFERS=$(cat /proc/meminfo | grep -w Buffers | awk {'print $2'})
-	MAPPED=$(cat /proc/meminfo | grep -w Mapped | awk {'print $2'})
-	SHMEM=$(cat /proc/meminfo | grep -w Shmem | awk {'print $2'})
-	SLAB=$(cat /proc/meminfo | grep -w Slab | awk {'print $2'})
-	if [ $LASTMEM -gt $MEMFREE  ]; then
-		echo $(date) Free memory: $MEMFREE MB cache: $CACHE kB buffers: $BUFFERS kB mapped: $MAPPED kB shmem: $SHMEM kB slab: $SLAB kB
+	CACHE=$(cat /proc/meminfo | grep -w Cached: | awk {'print $2'})
+	MEMFREE=$(cat /proc/meminfo | grep -w MemFree: | awk {'print $2'})
+	BUFFERS=$(cat /proc/meminfo | grep -w Buffers: | awk {'print $2'})
+	MAPPED=$(cat /proc/meminfo | grep -w Mapped: | awk {'print $2'})
+	SHMEM=$(cat /proc/meminfo | grep -w Shmem: | awk {'print $2'})
+	SLAB=$(cat /proc/meminfo | grep -w Slab: | awk {'print $2'})
+	ACTIVE=$(cat /proc/meminfo | grep -w Active: | awk {'print $2'})
+	INACTIV=$(cat /proc/meminfo | grep -w Inactive: | awk {'print $2'})
+	ACTIVEANON=$(cat /proc/meminfo | grep -w Active\(anon\): | awk {'print $2'})
+	INACTIVEANON=$(cat /proc/meminfo | grep -w Inactive\(anon\): | awk {'print $2'})
+	ACTIVEFILE=$(cat /proc/meminfo | grep -w Active\(file\): | awk {'print $2'})
+	INACTIVEFILE=$(cat /proc/meminfo | grep -w Inactive\(file\): | awk {'print $2'})
+	ACT=$(cat /proc/meminfo | grep -w $TRIGGER: | awk {'print $2'})
+	if [ "$TRIGGER" != "Buffers" ]; then
+		ACT=$(($ACT / $DIV))
+	fi
+	if [ $LAST -gt $ACT  ]; then
+		echo $(date) Free memory: $(($MEMFREE / $DIV)) MB cache: $CACHE kB buffers: $BUFFERS kB mapped: $MAPPED kB shmem: $SHMEM kB slab: $SLAB kB
+		echo ACTIVE: Active: $ACTIVE kB \($INACTIV kB\) Anon: $ACTIVEANON kB \($INACTIVEANON kB\) File: $ACTIVEFILE kB \($INACTIVEFILE kB\)
 		for p in $PIDLIST; do
 			if [ -f /proc/$p/statm ]; then
 				RSS=$(cat /proc/$p/statm | awk {'print $2'})
@@ -31,15 +51,8 @@ while [ 1 ]; do
 				echo $(cat /proc/$p/status | grep Name) pid: $p MEM: priv: $PRIV kB rss: $RSS kB shared: $SHR kB
 			fi
 		done
-		LASTMEM=$MEMFREE
+		LAST=$ACT
 		echo "--"
-	fi
-	DMESGLN=$(dmesg | tail -n 1)
-	if [ "$LASTDMESGLN" != "$DMESGLN" ]; then
-		echo "----- DMESG, last 20 lines -----"
-		dmesg | tail -n 20
-		echo "--------------------------------"
-		LASTDMESGLN=$DMESGLN
 	fi
 	sleep 1
 done
